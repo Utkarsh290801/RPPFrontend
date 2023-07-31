1d9c1bc689c220cf7cbf3275a5ae81cd6f8e16e1
Utkarsh290801 <utarora3@gmail.com>
2023-07-31 18:12:59 +0530

    f

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 //   backend_url: "http://localhost:5000",
 // };
 const app_config = {
-  backend_url: "http://16.171.24.1:5000",
+  backend_url: "https://16.171.24.1:5000",
 };
 
 export default app_config;
