0469c47724d8c7f2c9e281b1f8a49f7712547404
Utkarsh290801 <utarora3@gmail.com>
2023-08-02 01:23:38 +0530

    ..

--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
 // const app_config = {
 //   backend_url: "http://localhost:5000",
 // };
+// const app_config = {
+//   backend_url: "https://rpp-backend.vercel.app",
+// };
 const app_config = {
-  backend_url: "https://rpp-backend.vercel.app",
+  backend_url: "https://enasainc.co",
 };
 
 export default app_config;
